687519c5656100542beef42c23ccd95871b4c9e6
vdabral <vdabral21007@gmail.com>
2025-05-06 14:27:55 +0530

    Third commit

--- controllers/system.controllers.js
+++ controllers/system.controllers.js
@@ -1,1 +1,11 @@
-const getIpDetails = (req, res) => {};
+const dns = require("node:dns");
+const os = require("node:os");
+const getIpDetails = (req, res) => {
+  dns.lookup("", options, (err, address, family) =>
+    console.log("address: %j family: IPv%s", address, family)
+  );
+};
+const getsysteminfo = (req, res) => {
+  let arch = os.arch();
+  let freeMem = os.freemem();
+};
